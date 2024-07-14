7098c145e2cc921cf268b645ba8c929be545cbc5
Pratapuchai2056 <125111344+Pratapuchai2056@users.noreply.github.com>
2024-07-14 04:51:22 +0000

    updated datatype summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -33,3 +33,33 @@ const myFunction = function() {
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myGitHubName = "Pratapuchai2056"
+
+let anotherName = myGitHubName
+anotherName = "jimbohaha"
+
+console.log(myGitHubName);
+console.log(anotherName);
+
+// Stack memory gives copy of variable so as anotherName had copy of myGitHubName which later to "jimbohaha" prints changed variable and myGitHubName remains unchanged.
+
+let userOne = {
+    email: "user@google.com",
+    password: "user@kpo"
+}
+
+let userTwo = userOne
+
+userTwo.email = "pratap@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// Heap memory directly gives original reference from heap memory so as email in userTwo changed also changes userOne value while it gets printed.
+
