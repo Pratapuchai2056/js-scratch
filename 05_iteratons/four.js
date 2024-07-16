05b84621c018075bb595752732c61b60abbc15b7
Pratapuchai2056 <125111344+Pratapuchai2056@users.noreply.github.com>
2024-07-16 12:34:30 +0000

    for in loop basics

---
+++ 05_iteratons/four.js
@@ -0,0 +1,31 @@
+// for in loop
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+   // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('AUS', "Australia")
+// map.set('US', "United States")
+// map.set('UK', "United Kingdom")
+// map.set('AUS', "Australia")
+
+// for (const key in map) {
+//   console.log(key);
+// }
+
+
+
