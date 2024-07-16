f80024e8281a806b1337dc7056a079d0d557a4fb
Pratapuchai2056 <125111344+Pratapuchai2056@users.noreply.github.com>
2024-07-16 02:33:57 +0000

    control flow basics and scope and if else, conditions

---
+++ 04_control_flow/one.js
@@ -0,0 +1,49 @@
+// if 
+const isUserLoggedIn = true
+const temperature = 13
+
+// if ( temperature === 12 ) {
+// console.log("temperature is less than 15");
+// } else {
+// console.log("temperature is greater than 15");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+
+// console.log(`User Power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"), console.log("test2");
+
+// if ( balance < 500 ) {
+// console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2 == 3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail ) {
+    console.log("User logged in");
+}
