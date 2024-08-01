fed5caa0735642df9195a6e05affa255f7ca8334
PratapUT <pratapuchai2056@gmail.com>
2024-08-02 01:37:01 +1000

    promises basics

--- 09_bit_advancing/promises.js
+++ 09_bit_advancing/promises.js
@@ -3,9 +3,49 @@ const promiseOne = new Promise(function(resolve, reject){
     // DB calls, cryptography, network
     setTimeout(function(){
         console.log('Async task is complete');
+        resolve()
     }, 1000)
 })
 
 promiseOne.then(function(){
     console.log("Promise consumed");
+})
+
+new Promise (function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Pratap", email: "pratap@example.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(a){
+    console.log(a);
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error) {
+            resolve({username: "pratap", password: "123"})
+        } else {
+           reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+const username = promiseFour.then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
 })
