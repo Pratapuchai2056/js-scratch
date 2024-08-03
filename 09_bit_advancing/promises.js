c3305f2f6b5880ee1c8e288f8d2cda7a11b43022
PratapUT <pratapuchai2056@gmail.com>
2024-08-03 18:54:37 +1000

    promises

--- 09_bit_advancing/promises.js
+++ 09_bit_advancing/promises.js
@@ -53,16 +53,45 @@ promiseFour
 
 const promiseFive = new Promise(function(resolve, reject){
     setTimeout(function(){
-        let error = false
+        let error = true
         if (!error) {
             resolve({username:"javascript", password:"456"})
         } else {
             reject('Error: js went wrong')
         }
     }, 1000)
-})
+});
 
 async function consumePromiseFive () {
+   try {
     const response = await promiseFive
     console.log(response);
+   } catch (error) {
+    console.log(error);
+   }
 }
+
+consumePromiseFive()
+
+// async function getAllUsers(){
+//   try {
+//     const response=  await fetch('https://jsonplaceholder.typicode.com/posts')
+//     const data = await response.json()
+//     console.log(data);
+//   } catch (error) {
+//     console.log("Error:", error);
+//   }
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error);
+})
